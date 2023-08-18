f7daa4f27840cdd4cfc94762975505079d414d72
vincent <yunchenc@umich.edu>
2023-08-18 22:45:35 +0800

    updated and added description

--- Array/33_search_in_rotated_array.py
+++ Array/33_search_in_rotated_array.py
@@ -1,22 +1,26 @@
+# Time: O(log(n))
+# Space: O(1)
+# binary search: use left sorted and right sorted; make cases 
+# and update index as needed
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums)-1
+        l, r = 0, len(nums) - 1
 
         while l <= r:
-            mid = (l+r) // 2
-            if target == nums[mid]:
+            mid = (l+r)//2
+            if nums[mid] == target:
                 return mid
-            #left sorted
-            if nums[mid] >= nums[l]:
-                if target < nums[l] or target > nums[mid]:
+
+            # left sorted
+            if nums[mid] - nums[l] >= 0:
+                if nums[l] > target or target > nums[mid]:
                     l = mid + 1
                 else:
-                    r = mid - 1 
-            #right sorted
+                    r = mid - 1
             else:
-                if target > nums[r] or target < nums[mid]:
+                if target < nums[mid] or target > nums[r]:
                     r = mid - 1
                 else:
                     l = mid + 1
-
+        
         return -1
