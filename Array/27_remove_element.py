6852d89184ca2bb93a4277343d401c6f98bc0e05
vincent <yunchenc@umich.edu>
2023-09-28 00:52:47 -0400

    27 remove element

--- Array/27_remove_element.py
+++ Array/27_remove_element.py
@@ -3,10 +3,12 @@
 # l points to == val while r points to != val and swaps
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+
         l = 0
+        
         for r in range(len(nums)):
             if nums[r] != val:
-                nums[r], nums[l] = nums[l], nums[r]
+                nums[l] = nums[r]
                 l += 1
-
+            
         return l
