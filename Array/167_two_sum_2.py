d807c18a49814a2c6727b57f2646c14b6b196db5
vincent <yunchenc@umich.edu>
2023-10-06 10:50:05 -0400

    twosum

---
+++ Array/167_two_sum_2.py
@@ -0,0 +1,17 @@
+# Time: O(n)
+# Space: O(1)
+# l, r pointers: if over target, decrease r; else increase l
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            if numbers[l] + numbers[r] == target:
+                return [l+1, r+1]
+            elif numbers[l] + numbers[r] > target:
+                r -= 1
+            else:
+                l += 1
+        
+        return []
