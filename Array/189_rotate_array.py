77a2e9658bd06ded61b7cbe08106c6f2caa59e78
vincent <yunchenc@umich.edu>
2023-10-22 15:22:02 -0400

    brute force

--- Array/189_rotate_array.py
+++ Array/189_rotate_array.py
@@ -1,3 +1,18 @@
+# Time: O(n^2)
+# Space: O(1) 
+# Brute Force
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        for i in range(k):
+            last = nums [len(nums)-1]
+            for j in range(1, len(nums)):
+                nums[0], nums[j] = nums[j], nums[0]
+            nums[0] = last
+
 # Time: O(N)
 # Space: O(1) 
 class Solution:
