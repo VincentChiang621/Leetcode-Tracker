d0855d2d27a58679fa794ff627e41380d1621eef
vincent <yunchenc@umich.edu>
2023-10-22 21:57:26 -0400

    max product of 2 element in array

--- Stack/1464_max_product_of_2_elements_of_array.py
+++ Stack/1464_max_product_of_2_elements_of_array.py
@@ -10,4 +10,27 @@ class Solution:
                 maxP = max(maxP, product)
 
         return maxP
+
+# Time: O(nlogn)
+# Space: O(1) 
+# sort and then return last two elements product
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        return (nums[-1]-1) * (nums[-2]-1)
     
+# Time: O(n)
+# Space: O(1) 
+# l,r pointers at both ends and move closer
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        l, r = 0, len(nums)-1
+        maxP = 0
+        while l < r:
+            maxP = max(maxP, (nums[l]-1)*(nums[r]-1))
+            if nums[l] <= nums[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return maxP
