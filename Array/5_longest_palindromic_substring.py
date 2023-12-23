7796c7873f9ac04559d2c9429e1f87a69940a258
vincent <yunchenc@umich.edu>
2023-12-23 23:21:24 +0800

    longest palindrome sub

--- Array/5_longest_palindromic_substring.py
+++ Array/5_longest_palindromic_substring.py
@@ -1,1 +1,19 @@
-5. Longest Palindromic Substring
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+        def PaliCount(l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return r - l - 1
+
+        left, right = 0, 0
+        for i in range(len(s)):
+            len1 = PaliCount(i, i)
+            len2 = PaliCount(i, i + 1)
+            maxlen = max(len1, len2)
+            if maxlen > (right - left):
+                right = i + (maxlen // 2)
+                left = i - ((maxlen - 1) // 2)
+            
+        return s[left:right + 1]
