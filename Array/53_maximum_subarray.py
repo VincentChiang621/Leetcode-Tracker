139483a2aa419fbbab53057fc3ac2a8fc75c28b5
vincent <yunchenc@umich.edu>
2023-12-30 14:44:52 +0800

    max subarray

--- Array/53_maximum_subarray.py
+++ Array/53_maximum_subarray.py
@@ -3,13 +3,19 @@
 # if currentSum below 0, set to zero if above zero, add to result
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        currS = 0
-        maxSub = nums[0]
+        ans, currsum = -inf, 0
+        start, end = -1, -1
 
-        for n in nums:
-            if currS < 0:
-                currS = 0
-            currS += n
-            maxSub = max(maxSub, currS)
+        for i in range(len(nums)):
+            if currsum == 0:
+                start = i
+            currsum += nums[i]
+            if ans < currsum:
+                ans = currsum
+                end = i
 
-        return maxSub
+            if currsum < 0:
+                currsum = 0
+
+        print(nums[start:end+1])
+        return ans
