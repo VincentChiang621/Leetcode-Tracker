78bb32bb1241e66277aa8d7047f16121865e979f
Vincent <yunchenc@umich.edu>
2024-02-07 23:49:09 -0500

    find min

--- Array/153_find_min_in_rotated_array.py
+++ Array/153_find_min_in_rotated_array.py
@@ -15,3 +15,19 @@ class Solution:
                 l = m + 1
             
         return ans
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        smallest = nums[0]
+
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] >= nums[l]:
+                smallest = min(smallest, nums[l])
+                l = m + 1
+            else:
+                smallest = min(smallest, nums[m])
+                r = m - 1
+        
+        return smallest
